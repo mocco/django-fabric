de77bbb367e4e3fef11fd1444c44cf309ceb4de5
Rolf Erik Lekang <me@rolflekang.com>
2013-08-14 14:36:41 +0200

    add more meta to setup.py and bump version number

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-fabric',
-    version='1.0.1',
+    version='1.1',
     author='Rolf Erik Lekang',
     author_email='rolf@mocco.no',
+    url='http://mocco.no/django-fabric/',
     packages=find_packages(),
-
+    description='a generic fabric utility class for django projects',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['fabric']
+    install_requires=['fabric', 'django']
 )
