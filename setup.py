1a34e347b33e023d74a0e513b3802742741e6d65
Rolf Erik Lekang <me@rolflekang.com>
2013-09-08 22:49:36 +0200

    bump version number to 1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-fabric',
-    version='1.2',
+    version='1.3',
     author='Rolf Erik Lekang',
     author_email='rolf@mocco.no',
     url='http://mocco.no/django-fabric/',
