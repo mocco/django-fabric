bab0ae9f851e21856483d0ebab81ccac6dc23457
Rolf Erik Lekang <me@rolflekang.com>
2014-04-09 19:39:15 -0700

    pep8

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
+# import sys
+# import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -187,22 +187,22 @@ htmlhelp_basename = 'django-fabricdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'django-fabric.tex', u'django-fabric Documentation',
-   u'Rolf Erik Lekang', 'manual'),
+    ('index', 'django-fabric.tex', u'django-fabric Documentation',
+     u'Rolf Erik Lekang', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -245,9 +245,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'django-fabric', u'django-fabric Documentation',
-   u'Rolf Erik Lekang', 'django-fabric', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'django-fabric', u'django-fabric Documentation',
+     u'Rolf Erik Lekang', 'django-fabric', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
